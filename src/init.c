e0bca213ef1696331dbbf88aa18b651daca222f7
Voltro <voltro.oc@gmail.com>
2026-03-14 13:00:12 +0200

    Windows compatibility test 2

--- src/init.c
+++ src/init.c
@@ -16,13 +16,95 @@
 
 bool init(){
 
-
-    const char* dir = "test_folder";
+    const char* dir = ".chz";
+    const short perm = 0700;
+    const char *directories[] = {"refs", "hooks" , "info", "logs", "objects", "objects/info"};
+    const char *files[] = {"HEAD","index", "config", "description", "packed-refs"};
 
     #ifdef _WIN32
-        mkdir(dir);
+
+        DIR* dir = opendir(dir);
+        if(dir)
+        {
+            printf(".chz folder already exists")
+            colsedir(dir);
+            return false;
+        }
+
+        if(mkdir(dir) == -1)
+        {
+          printf("mkdir %s, failed",dir);
+          exit(EXIT_FAILURE);
+        }
+
+        size_t size = sizeof(directories) / sizeof(directories[0]);
+
+        for(size_t i = 0; i < size; i++)
+        {
+            char path[256];
+            snprintf(path, sizeof(path), ".chz/%s", directories[i]);
+            if(mkdir(path) < 0) 
+            {
+                printf("mkdir failed on: %s", path);
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        size = sizeof(files) / sizeof(files[0]);
+
+        for(size_t i = 0; i < size; i++)
+        {
+            char path[256];
+            snprintf(path, sizeof(path), ".chz/%s", files[i]);
+            if(fopen(path, "w") < 0) 
+            {
+                printf("fopen failed on: %s", path);
+                exit(EXIT_FAILURE);
+            }
+        }
+
     #else
-        mkdir(dir, 0755);
+        
+        DIR *dr = opendir(dir);
+        if(dr)
+        {
+            perror(".chz folder exits");
+            closedir(dr);
+            return false;
+        }
+
+        if(mkdir(dir, 0755)== -1)
+        {
+            perror(".chz folder creation error");
+            closedir(dr);
+            return false;
+        }
+        size_t size = sizeof(directories) / sizeof(directories[0]);
+
+        for(size_t i = 0; i < size; i++)
+        {
+            char path[256];
+            snprintf(path, sizeof(path), ".chz/%s", directories[i]);
+            if(mkdir(path, perm) < 0) 
+            {
+                printf("mkdir failed on: %s", path);
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        size = sizeof(files) / sizeof(files[0]);
+
+        for(size_t i = 0; i < size; i++)
+        {
+            char path[256];
+            snprintf(path, sizeof(path), ".chz/%s", files[i]);
+            if(fopen(path, "w") < 0) 
+            {
+                printf("fopen failed on: %s", path);
+                exit(EXIT_FAILURE);
+            }
+        }
+
     #endif
 
     return true;
