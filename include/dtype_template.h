66306aa961b296b12b16b33159ad645b80380beb
ElementaleLord <patrickfarah200515@outlook.com>
2026-03-14 20:22:04 +0200

    initial Datatype atempt

---
+++ include/dtype_template.h
@@ -0,0 +1,140 @@
+#ifndef DTYPE_TEMPLATE_H
+#define DTYPE_TEMPLATE_H
+
+#include <time.h>
+#include <string.h>
+
+struct Commit;
+struct Branch;
+struct Repository;
+
+enum FileStatus
+{
+    MODIFIED, 
+    UNMODIFIED, 
+    REMOVED, 
+    ADDED
+};
+enum AthenticationMethod
+{
+    SECONDARY_EMAIL, 
+    PHONE_NUMBER, 
+    AUTHENTICATOR_APP
+};
+enum ActionType
+{
+    CREATE, 
+    DELETE, 
+    VIEW
+};
+enum PullRequestCategory
+{
+    OPEN, 
+    CLOSED
+};
+enum PullRequestStatus
+{
+    ACCEPTED, 
+    REJECTED, 
+    APPLIED, 
+    NUL
+};
+
+typedef struct
+{// a file with a status attached
+    FileStatus status;
+    char* FileName;
+}StatusFile;
+
+typedef struct
+{// a file with a date attached
+    time_t date;
+    long* fileObj;
+}DateFile;
+
+typedef struct
+{// used to track the category and status of a pull request
+    PullRequestCategory pullReqCat;
+    PullRequestStatus pullReqStatus;
+}PullRequestType;
+
+typedef struct
+{// defines a name and points to some commit
+    char* label;
+    Commit* commitPtr;
+}LightTag;
+
+typedef struct
+{// expansion on light tag that additionally track date and user which created this tag
+    char* label;
+    Commit* commitPtr;
+    Account* creater;
+    time_t HTagDate;
+    char* HTagMsg;
+}HeavyTag;
+
+typedef struct
+{// General data type for a Repository
+    char* repoName;
+    time_t repoDate;
+    char* repoURL;
+    long icon;
+    RepositoryLogEntry* repoLog;// array of log entries (i think we defined this to make git log easier to code? maybe)
+    LightTag* ptrMap;// array of light tags
+    HeavyTag* tagMap;// array of heavy tags
+    Branch** branches;// array of branch pointers
+}Repository;
+
+typedef struct
+{// General data type for a Branch
+    char* branchName;
+    time_t branchDate;
+    DateFile* branchData;
+    Repository* parentRepo;
+    Commit* latestCommit;
+}Branch;
+
+typedef struct
+{// General data type for a Commit
+    char* commitName;
+    char* commitMsg;
+    time_t commitDate;
+    Branch* commitBranch;
+    Branch* mergeBranch;
+    StatusFile* commitedChanges;
+}Commit;
+
+typedef struct
+{//? Account Settings Data Type
+    char* email;
+    long password;// should be a hashed ref
+    char* username;
+    bool twoFactorAuth;
+    AthenticationMethod method;
+}Account;
+
+typedef struct
+{// used as one entry in a repositories history log
+    time_t logEntrydate;
+    Commit* RepLogData;
+}RepositoryLogEntry
+;
+
+typedef struct
+{// defines the extra data needed by a forked repository
+    char* originalURL;
+    time_t forkDate;
+    long vNum
+}RepoFork;
+
+typedef struct
+{// General data type for a  PullRequest
+    char* pullRequestName;
+    char* pullRequestMsg;
+    time_t createDate;
+    time_t submitDate;
+    time_t resolveDate;
+
+}PullRequest;
+
+#endif
---
+++ include/test.c
@@ -0,0 +1,8 @@
+#include "dtype_template.h"
+
+int main(){
+    Commit c;
+    Branch b;
+    Repository r;
+    return 0;
+}
