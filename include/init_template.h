957d7547bad9ac342d5436a27e544df7677fc8e8
Faust <cfares405@gmail.com>
2026-03-14 17:06:33 +0200

    refactor of file reading (linux)

--- include/init_template.h
+++ include/init_template.h
@@ -1,5 +1,5 @@
-#ifndef INIT_TEMPLATE
-#define INIT_TEMPLATE
+#ifndef INIT_TEMPLATE_H
+#define INIT_TEMPLATE_H
 
 #include <stddef.h>
 
@@ -9,10 +9,22 @@ typedef struct
     const char* data;
 }TemplateValue;
 
-const TemplateValue REPO_TEMPLATE[] = {
+static const TemplateValue REPO_TEMPLATE[] = {
 
     {".chz", NULL },
-    {".chz/refs", NULL}
+    {".chz/refs", NULL},
+    {".chz/refs/heads", NULL},
+    {".chz/refs/tags", NULL},
+    {".chz/objects", NULL},
+    {".chz/objects/info", NULL},
+    {".chz/hooks", NULL},
+    {".chz/info", NULL},
+    {".chz/HEAD", "ref: refs/heads/main\n"},
+    {".chz/index", ""},
+    {".chz/config", "[core]\n\trepositoryformatversion = 0\n"},
+    {".chz/description", "Unnamed repository\n"},
+    {".chz/unpacked-refs", ""},
 };
 
+static const size_t REPO_TEMPLATE_SIZE = sizeof(REPO_TEMPLATE)/sizeof(REPO_TEMPLATE[0]);
 #endif
--- src/init.c
+++ src/init.c
@@ -4,9 +4,10 @@
 #include <errno.h>
 #include <stdbool.h>
 #include <string.h>
-#include "include/init_template.h"
 
-//seperate both mdkir commands because function overriding doesnt exist in C
+// two dots to go up a dir
+#include "../include/init_template.h"
+
 #ifdef _WIN32
 #include <direct.h>
 #define mkdir(dir) _mkdir(dir)
@@ -19,16 +20,6 @@ bool init()
 {
     const char* dir = ".chz";
     const short perm = 0700;
-    const char *directories[] = {"refs", "hooks" , "info", "logs", "objects", "objects/info"};
-    const char *files[] = {"HEAD","index", "config", "description.txt", "packed-refs"};
-
-    const char *content[] = {
-        "", 
-        "", 
-        "c1\nc2",
-        "desc",
-        ""
-    };
 
     DIR* p_dir = opendir(dir);
     if(p_dir)
@@ -80,44 +71,30 @@ bool init()
 
     #else
 
-        if(mkdir(dir, 0755)== -1)
+        for(size_t i = 0; i < REPO_TEMPLATE_SIZE; i++)
         {
-            perror(".chz folder creation error");
-            closedir(p_dir);
-            return false;
-        }
-        size_t size = sizeof(directories) / sizeof(directories[0]);
+            const char* data =  REPO_TEMPLATE[i].data;
+            const char* path = REPO_TEMPLATE[i].path;
 
-        for(size_t i = 0; i < size; i++)
-        {
-            char path[256];
-            snprintf(path, sizeof(path), ".chz/%s", directories[i]);
-            if(mkdir(path, perm) < 0) 
+            if(data == NULL)
             {
-                printf("mkdir failed on: %s", path);
-                exit(EXIT_FAILURE);
+                if(mkdir(path,perm) < 0)
+                {
+                    printf("failed in creating %s" , path);
+                    exit(EXIT_FAILURE);
+                }
+            }else
+            {
+                FILE *fp = fopen(path,"w");
+                if(!fp)
+                {
+                    printf("failed in opening file %s", path);
+                    exit(EXIT_FAILURE);
+                }
+                fwrite(data, 1, strlen(data), fp);
+                fclose(fp);
             }
         }
-
-        size = sizeof(files) / sizeof(files[0]);
-
-        for(size_t i = 0; i < size; i++)
-        {
-            FILE *fp;
-            char path[256];
-            snprintf(path, sizeof(path), ".chz/%s", files[i]);
-            if((fp = fopen(path, "w")) < 0) 
-            {
-                printf("fopen failed on: %s", path);
-                exit(EXIT_FAILURE);
-            }
-            if(strcmp(content[i], ""))
-            {
-                fputs(content[i],fp);
-            }
-            fclose(fp);
-        }
-
     #endif
 
     return true;
