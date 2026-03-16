bb3e11256444ad1dbb6959a0cc92c754588b1741
Faust <cfares405@gmail.com>
2026-03-16 21:23:15 +0200

    fixed connection with backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,10 +4,14 @@ const app = express();
 const PORT = 3000;
 
 app.use(cors());
-app.use(express);
+app.use(express.json());
 
-app.get("api/hello", (req, res) => {
-    res.json({ message: "Backend says your ssn is 751 26 9855" });
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.get("/api/hello", (req, res) => {
+    res.json({ message: "Backend says hi " });
 });
 
 app.listen(3000, () => console.log("server running on port 3000"));
