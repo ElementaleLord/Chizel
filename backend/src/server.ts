f3cd08f0dfe10b683d0cfba7027c693c0929a101
Faust <cfares405@gmail.com>
2026-03-16 18:14:40 +0200

    backend runs C code and creates files when needed, init test passed, testing is required on windows

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,36 @@
 import express, { Request, Response } from 'express';
+<<<<<<< HEAD
+import cEngine from "./c-engine";
+
+const app = express();
+const PORT = 3000;
+app.use("/api", cEngine);
+
+app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeader(
+        "Content-Security-Policy",
+        "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self'"
+    );
+    next();
+});
+=======
 import chzRouter from "./c-engine";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+>>>>>>> 00b3c8e377521d264bd90ab4944b88ef60fe4c04
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello Backend!');
 });
 
+<<<<<<< HEAD
+=======
 app.use("/api", chzRouter);
+>>>>>>> 00b3c8e377521d264bd90ab4944b88ef60fe4c04
 
 app.listen(PORT, () => {
     console.log (`server running on http://localhost:${PORT}`);
