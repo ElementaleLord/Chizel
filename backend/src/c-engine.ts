b07d1cb21576c0852203420d5c0cc2baf93d3ae6
Faust <cfares405@gmail.com>
2026-03-16 18:14:40 +0200

    backend runs C code and creates files when needed, init test passed, testing is required on windows

--- backend/src/c-engine.ts
+++ backend/src/c-engine.ts
@@ -1,4 +1,30 @@
 import { spawn } from "child_process";
+<<<<<<< HEAD
+import { Router } from "express";
+import path = require("path");
+import fs from "fs";
+import { exit } from "process";
+
+const router = Router();
+const cwd = path.resolve("/tmp/test_repo");
+console.log("Does CWD exist?", fs.existsSync(cwd));
+if(!fs.existsSync(cwd)) fs.mkdirSync(cwd, { recursive: true });
+
+const init = path.resolve(__dirname, "../../build/init");
+console.log("Binary path:", init);
+
+router.get("/c_engine", (req, res) => {
+
+    const absolutInit = path.resolve(__dirname, "../../build/init");
+
+    console.log("--- DEBUG START ---");
+    console.log("Binary exists at: ", absolutInit, fs.existsSync(absolutInit));
+    console.log("Target cwd: ", cwd);
+    console.log("target cwd exists: ", fs.existsSync(cwd));
+    console.log("--- DEBUG END ---");
+
+    const process = spawn(init,[],{cwd});
+=======
 import { Router } from "express"
 
 const router = Router();
@@ -6,6 +32,7 @@ const router = Router();
 router.get("/chiz_engine", (req, res) => {
 
     const process = spawn("../../build/init",[], {cwd: "tmp/testrepo"});
+>>>>>>> 00b3c8e377521d264bd90ab4944b88ef60fe4c04
 
     let output = "";
 
