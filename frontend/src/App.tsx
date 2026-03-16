e49af7a80075391b2134fa9b727a04167d317ce1
Faust <cfares405@gmail.com>
2026-03-16 23:11:17 +0200

    Work in process running C code from frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,30 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 import './App.css'
 
 function App() 
 {
     const [msg, setMsg] = useState("");
-
-    useEffect(() => {
+    const fetchHello = () => {
         axios.get("http://localhost:3000/api/hello")
-        .then(response => setMsg(response.data.message))
+            .then(response => setMsg(response.data.message))
+            .catch(err => console.error(err));
+    };
+
+    const chzInit = () => {
+        setMsg("running binary...");
+        axios.post("http://localhost:3000/api/c_engine", { message : 'init' })
+        .then(response => setMsg(response.data.result))
         .catch(err => console.error(err));
-    }, []);
+    };
     
-    return <div>{msg}</div>
+    return (
+        <div>
+            <button onClick = {fetchHello}> Call Api </button>
+            <button onClick = {chzInit}> Make Repo </button>
+            <div>{msg}</div>
+        </div>
+    );
 }
 
 export default App;
