34b80a1ff8517ecd1eb112fac903fc35b5118a65
Voltro <voltro.oc@gmail.com>
2026-03-17 20:36:55 +0200

    LCS IS DONE BABYYYYYYYYYYYYYYYYYYYY

--- include/lcs_algos.h
+++ include/lcs_algos.h
@@ -1,16 +1,14 @@
+#include <sys/types.h>
+
 #ifndef LCS_ALGOS_H
 #define LCS_ALGOS_H
 
-typedef struct
-{
-    int start_old;
-    int start_new;
-    int lines_removed;
-    int lines_added; 
-}Changes;
+typedef struct{
+    char** lines;
+    size_t capacity;
+    size_t size;
+} Lines;
 
-int max(int x, int y);
-int lcsRec(char* s1, char* s2, int m, int n);
-int lcs(char* s1, char* s2);
+int lcs(Lines s1, Lines s2);
 
 #endif
--- src/libchz/lcs_lib.c
+++ src/libchz/lcs_lib.c
@@ -31,14 +31,69 @@ Lines read_file(FILE* f)
     return result;
 }
 
+char **lcs_backtrack(Lines new_file, Lines old_file, int lcs_table[old_file.size+1][new_file.size+1], int len)
+{
+    size_t i = old_file.size;
+    size_t j = new_file.size;
+    int k = len - 1;
+    char **lcs_lines = malloc(len * sizeof(char *));
+
+    while(i > 0 && j > 0)
+    {
+        if(strcmp(old_file.lines[i-1], new_file.lines[j-1]) == 0)
+        {
+            printf(" %s", old_file.lines[i-1]);
+            i--;
+            j--;
+        }
+        else if(lcs_table[i-1][j] >= lcs_table[i][j-1]) 
+        {
+            printf("- %s", old_file.lines[i-1]);
+            i--;
+        }
+        else 
+        {
+            printf("+ %s", new_file.lines[j-1]);
+            j--;
+        }
+        
+        while(i > 0) { printf("- %s",old_file.lines[i - 1]); i--;} 
+        while(j > 0) { printf("+ %s",old_file.lines[j - 1]); j--;} 
+    }
+
+    return lcs_lines;
+}
+
 int lcs(Lines new_file, Lines old_file)
 {
-    int index_new = 0;
-    int index_old = 0;
+    int lcs_table[old_file.size + 1][new_file.size + 1];
 
-    while(index_new <= new_file.size ||  index_old <= old_file.size)
+    //lcs of first file vs line 0 of new file
+    for(int i = 0; i < old_file.size; i++)
     {
+        lcs_table[i][0] = 0;
     }
+    
+    //lcs of new file vs line 0 of old file
+    for(int j = 0; j < old_file.size; j++)
+    {
+        lcs_table[0][j] = 0;
+    }
+
+    
+    for(int i = 1; i <= old_file.size; i++)
+    {
+        for(int j = 1; j <= new_file.size; j++)
+        {
+            if(strcmp(old_file.lines[i-1], new_file.lines[j-1])) lcs_table[i][j] = lcs_table[i - 1][j - 1] + 1;
+            else lcs_table[i][j] = max(lcs_table[i-1][j], lcs_table[i][j-1]);
+        }
+    }
+
+    int len = lcs_table[old_file.size][new_file.size];
+    char **lcs_lines = lcs_backtrack(new_file, old_file, lcs_table, len);
+
+    return len;
 }
 
 int main()
@@ -60,6 +115,9 @@ int main()
     }
     Lines old_file_lines = read_file(old_file);
     fclose(old_file);
+
+    int result = lcs(new_file_lines, old_file_lines);
+    printf("LCS: %d\n", result);
 
     return 0;
 }
